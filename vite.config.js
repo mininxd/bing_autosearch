f5ef10762ebcfcebe4aa272a410a4b67f1858176
mininxd <mininsna@gmail.com>
2025-11-14 08:52:42 +0000

    yay 1.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import packages from './package.json'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  define: {
+    APP_VER: JSON.stringify(packages.version),
+  },
 })
