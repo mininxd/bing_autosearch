329a731f6c62923fa63d55bc998702b35c10a062
mininxd <mininsna@gmail.com>
2025-11-19 03:02:15 +0000

    image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import packages from './package.json'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    ViteImageOptimizer({
+      png: {
+        compressionLevel: 9,
+        quality: 1,
+        palette: true
+      }
+    })
   ],
   define: {
     APP_VER: JSON.stringify(packages.version),
   },
+  build: {
+    cssMinify: true,
+    sourcemap: false,
+    reportCompressedSize: true,
+    brotliSize: true,
+  }
 })
